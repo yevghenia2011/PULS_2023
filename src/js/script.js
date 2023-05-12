ba2a3df04cc06106099391fe3335606297d3d637
yevghenia2011 <yevghenia2011@gmail.com>
2023-05-12 11:57:09 +0300

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,86 @@
+$(document).ready(function(){
+    $('.choice__slider').slick({
+        speed: 1200,
+       /*  adaptiveHeight: true, */
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/red_left-solid.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/red_right-solid.png"></button>',
+        responsive: [
+            {
+              breakpoint: 992,
+              settings: {
+                dots: true,
+                arrows: false
+              }
+            }
+        ]
+    });
+
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+      $(this)
+        .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+        .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+       });
+
+    function toggleSlide(item) {
+      $(item).each(function(i) {
+        $(this).on('click', function(e) {
+          e.preventDefault();
+          $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+          $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+        });
+      });
+    }
+
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
+
+    //Modal
+
+    $('[data-modal=consultation]').on('click', function() {
+      $('.overlay, #consultation').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function() {
+      $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+    });
+
+    $('[data-modal=order]').each(function(i) {
+      $(this).on('click', function() {
+          $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+          $('.overlay, #order').fadeIn('slow');
+      });
+    });
+
+  function valideForms(form){
+    $(form).validate({
+      rules: {
+        name: {
+          required: true,
+          minlength: 5,
+        },
+        phone: "required",
+        email: {
+          required: true,
+          email: true
+        }
+      },
+      messages: {
+        name: {
+          required: "Пожалуйста, введите свое имя",
+          minlength: jQuery.validator.format("Введите {5} символов!"),
+        },
+        phone: "Пожалуйста, введите свой номер телефона",
+        email: {
+          required: "Пожалуйста, введите свою почту",
+          email: "Неправильно введен адрес почты"
+        } 
+      }
+    });
+  }
+
+  valideForms("#consultation-form");
+  valideForms("#consultation form");
+  valideForms("#order form");
+
+  $("input[name=phone]").mask("+3 (999) 999-9999");
+
+});
